9f96a467d13e4221430df7a26652d8fe2a0c6d32
Adrian Bjune <afbjune@gmail.com>
2018-11-10 14:48:52 -0800

    changed label_docs ouput style

--- toolkit.py
+++ toolkit.py
@@ -5,33 +5,37 @@ import pandas as pd
 
 
 def label_docs(docs):
-	'''
-	Takes in a list of strings and prompts user on whether each
-	string matches a binary label.
+    '''
+    Takes in a list of strings and prompts user on whether each
+    string matches a binary label.
 
-	Input:
-	Numpy array - List of documents to be labelled
+    Input:
+    Numpy array - List of documents to be labelled
 
-	Output: 
-	Numpy array - list of binary results of whether each document
-				  matches the desired label.
-	'''
-	getch = _Getch()
+    Output: 
+    Numpy array - list of binary results of whether each document
+      matches the desired label.
+    '''
+    getch = _Getch()
     count = len(docs)
-	output_array = np.zeros(count)
+    output_array = np.zeros(lcount)
 
-	for i, doc in enumerate(docs):
-		print(doc)
-		while True:
-			response = getch()
-			if response == '[':
-				output_array[i] = 1
-				break
-			elif response == ']':
-				break
-        print('{}/{} completed.'.format(i, count))
-		print()
-	return output_array
+    for i, doc in enumerate(docs):
+        print(doc)
+        while True:
+            response = getch()
+            if response == '[':
+                output_array[i] = 1
+                break
+            elif response == ']':
+                break
+            elif response == ['q']:
+                break
+        if response == ['q']:
+            break
+        print('{}/{} completed.'.format(i+1, count))
+        print()
+    return output_array, i+1
 
 
 class _Getch:
