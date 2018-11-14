8f19a36658c8a12198b9fdba766b43e42a7eaff3
Adrian Bjune <afbjune@gmail.com>
2018-11-14 15:01:46 -0800

    added spark submission script

---
+++ spark_submit_script.sh
@@ -0,0 +1,8 @@
+${SPARK_HOME}/bin/spark-submit \
+--conf spark.sql.warehouse.dir="file:///tmp/spark-warehouse" \
+--master yarn \
+--deploy-mode cluster \
+--packages com.databricks:spark-csv_2.11:1.5.0 \
+--packages com.amazonaws:aws-java-sdk-pom:1.10.34 \
+--packages org.apache.hadoop:hadoop-aws:2.7.3 \
+$1
